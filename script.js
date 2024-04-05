62853997210c14985511a5c130f8d8c4ab06e6f8
rohan khandar <rohankhandar10@gmail.com>
2024-04-05 22:16:14 +0530

    first git

---
+++ script.js
@@ -0,0 +1,58 @@
+//console.log(" i am astudent");
+
+//var name=prompt(" i am taking input")
+//console .log (name)
+
+//for(let i=1; i<=10;i++)
+
+    // console.log(i);
+//}
+//let n=1;
+//while(n<=10)
+//{
+    // console.log(n);
+    //n++;
+    //}
+
+//   let n=prompt("enter a number")  ;
+//     if (n%2==0)
+//     {
+//         console.log("I am even");
+
+//     }
+// else {
+//     console.log("i am odd");
+// }
+
+// let marks=prompt("Enter your marks")
+// if (marks>80 && marks<=90)
+// {
+//     console.log("A grade");
+
+// }
+// else if (marks>70 && marks<80)
+// {  console.log("B grade");
+// }
+// else if (marks>60  && marks<=70)
+// {
+//     console.log("C grade")
+
+// }
+// else{
+//     console.log("D grade")
+// }
+
+// let fname=prompt("give fname");
+// let lname=prompt("give lname");
+// let msg="Hello"+ fname+""+lname+" you are using JS!!"
+// alert(msg);
+// guess the movie
+
+
+const favmovie="avengers"
+let guess=prompt(" guess my favourite movie");
+
+while((guess!=favmovie) && (guess="quit"))
+{
+guess=prompt("Wrong answer!! please try again!!");
+}
